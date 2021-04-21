8b7f87d8d9aa8a9a9b1c749ffb78d2996fcb5a06
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2021-04-21 10:39:38 +0530

    Add : prop types to react component

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', 'jest.config.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
   },
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  collectCoverageFrom: ['<rootDir>/pages', '**/*.{js,jsx}'],
+  coveragePathIgnorePatterns: ['<rootDir>/pages/api', '<rootDir>/coverage/', 'jest.config.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 33,
+      lines: 25,
+      statements: 40,
+    },
   },
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import PropTypes from 'prop-types';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.any.isRequired,
+};
 
 export default MyApp;
